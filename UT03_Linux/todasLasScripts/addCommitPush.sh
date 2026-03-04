423ca2e99968afc84add46bae107ef967a79822b
Cbas <sebastianlaya01@gmail.com>
2026-03-04 17:02:15 +0000

    prueba

---
+++ UT03_Linux/todasLasScripts/addCommitPush.sh
@@ -0,0 +1,13 @@
+#==============================================#
+#                                              #
+#    author: slayaglez                         #
+#    github: https://github.com/slayaglez      #
+#    version: 1.0.0                            #
+#                                              #
+#==============================================#
+
+#!/bin/bash
+
+git add .
+git commit -m "$1"
+git push
---
+++ UT03_Linux/todasLasScripts/pruebaAlias.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+alias PRO='cd ~/Escritorio/PRO-1-DAM-2025-2026'
